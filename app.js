0401eff4a8aef81842dcc649efeb47755bb36d1a
rkrishnav21 <rkrishna.v21@gmail.com>
2016-06-05 20:46:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-
+//edited by ramya
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
